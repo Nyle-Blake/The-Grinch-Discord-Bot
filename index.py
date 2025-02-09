164b18ae735e29845a3d24f30c6f3cb59436074e
Nyle-Blake <Nyleblake1@gmail.com>
2025-02-09 04:17:02 +0000

    Feat: add try except block to handle errors

--- index.py
+++ index.py
@@ -10,6 +10,14 @@ discord_token = os.getenv('DISCORD_TOKEN')
 giphy_token = os.getenv('GIPHY_TOKEN')
 main_guild = os.getenv('MAIN_GUILD')
 
+
+async def get_gif(query):
+    response = requests.get('https://api.giphy.com/v1/gifs/translate', params={'api_key': giphy_token, 's': query})
+    data = json.loads(response.text)
+    gif_url = data['data']['images']['original']['url']
+    return gif_url
+
+
 class MyClient(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
@@ -24,24 +32,31 @@ class MyClient(discord.Client):
         for guild in self.guilds:
             for channel in guild.text_channels:
                 if str(channel) == "general":
-                    await channel.send('Bot Activated..')
+                    await channel.send('*** The Ginch Has Arrived ***')
                     await channel.send(file=discord.File('the_grinch_intro.gif'))
         print('Active in {}\n Member Count : {}'.format(guild.name, guild.member_count))
 
+
 client = MyClient()
+
 
 @client.tree.command(name="gif", description="Get a gif", guild=discord.Object(id=main_guild))
 async def slash_command(interaction: discord.Interaction, query: str):
-    response = requests.get('https://api.giphy.com/v1/gifs/translate', params={'api_key': giphy_token, 's': query})
-    data = json.loads(response.text)
-    gif_url = data['data']['images']['original']['url']
-    await interaction.response.send_message(gif_url)
+    try:
+        gif_url = await get_gif(query)
+        await interaction.response.send_message(gif_url)
+    except:
+        await interaction.response.send_message('No gif found')
 
 @client.tree.command(name="ginch", description="Summon the ginch", guild=discord.Object(id=main_guild))
 async def slash_command(interaction: discord.Interaction, query: str):
-    if interaction.content == 'ginch':
+    if query == 'ginch':
         channel = interaction.channel
-        for i in range(5):
-            await channel.send('Ginch')
+        try:
+            for i in range(5):
+                await channel.send('*** I have been summoned ***')
+        except:
+            await interaction.response.send_message('*** Error summoning the ginch ***')
+
 
 client.run(discord_token)
