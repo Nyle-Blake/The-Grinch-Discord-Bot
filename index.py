3c20c2a79a0b0aee932c8adc0f378872bfdf667e
Nyle-Blake <Nyleblake1@gmail.com>
2025-02-12 16:33:41 +0000

    Fix: Puts tokens and env details into Myclient class
    This centralises data so that we can move our slash commands into a
    separate file and still have access to the tokens and env details that
    relate to this client.

--- index.py
+++ index.py
@@ -6,13 +6,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-discord_token = os.getenv('DISCORD_TOKEN')
-giphy_token = os.getenv('GIPHY_TOKEN')
-main_guild = os.getenv('MAIN_GUILD')
 
 
-async def get_gif(query):
-    response = requests.get('https://api.giphy.com/v1/gifs/translate', params={'api_key': giphy_token, 's': query})
+async def get_gif(query, api_key):
+    response = requests.get('https://api.giphy.com/v1/gifs/translate', params={'api_key': api_key, 's': query})
     data = json.loads(response.text)
     gif_url = data['data']['images']['original']['url']
     return gif_url
@@ -22,9 +19,12 @@ class MyClient(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.tree = app_commands.CommandTree(self)
+        self.discord_token = os.getenv('DISCORD_TOKEN')
+        self.giphy_token = os.getenv('GIPHY_TOKEN')
+        self.main_guild = os.getenv('MAIN_GUILD')
     
     async def on_ready(self):
-        await self.tree.sync(guild=discord.Object(id=main_guild))
+        await self.tree.sync(guild=discord.Object(id=self.main_guild))
         print(f'Logged in as {self.user}')
         await self.send_intro()
 
@@ -40,15 +40,15 @@ class MyClient(discord.Client):
 client = MyClient()
 
 
-@client.tree.command(name="gif", description="Get a gif", guild=discord.Object(id=main_guild))
+@client.tree.command(name="gif", description="Get a gif", guild=discord.Object(id=client.main_guild))
 async def slash_command(interaction: discord.Interaction, query: str):
     try:
-        gif_url = await get_gif(query)
+        gif_url = await get_gif(query, client.giphy_token)
         await interaction.response.send_message(gif_url)
     except:
         await interaction.response.send_message('No gif found')
 
-@client.tree.command(name="ginch", description="Summon the ginch", guild=discord.Object(id=main_guild))
+@client.tree.command(name="ginch", description="Summon the ginch", guild=discord.Object(id=client.main_guild))
 async def slash_command(interaction: discord.Interaction, query: str):
     if query == 'ginch':
         channel = interaction.channel
@@ -58,7 +58,7 @@ async def slash_command(interaction: discord.Interaction, query: str):
         except:
             await interaction.response.send_message('*** Error summoning the ginch ***')
 
-@client.tree.command(name="kick", description="Kick a member of the server", guild=discord.Object(id=main_guild))
+@client.tree.command(name="kick", description="Kick a member of the server", guild=discord.Object(id=client.main_guild))
 async def slash_command(interaction: discord.Interaction, member: discord.Member, reason: str):
     try:
         if reason == '':
@@ -70,7 +70,7 @@ async def slash_command(interaction: discord.Interaction, member: discord.Member
     except:
         await interaction.response.send_message('*** Error kicking the member ***')
 
-@client.tree.command(name="ban", description="Ban a member of the server", guild=discord.Object(id=main_guild))
+@client.tree.command(name="ban", description="Ban a member of the server", guild=discord.Object(id=client.main_guild))
 async def slash_command(interaction: discord.Interaction, member: discord.Member, reason: str):
     try:
         if reason == '':
@@ -82,7 +82,7 @@ async def slash_command(interaction: discord.Interaction, member: discord.Member
     except:
         await interaction.response.send_message('*** Error banning the member ***')
 
-@client.tree.command(name="unban", description="Unban a member of the server", guild=discord.Object(id=main_guild))
+@client.tree.command(name="unban", description="Unban a member of the server", guild=discord.Object(id=client.main_guild))
 async def slash_command(interaction: discord.Interaction, member: discord.Member, reason: str):
     try:
         if reason == '':
@@ -94,7 +94,7 @@ async def slash_command(interaction: discord.Interaction, member: discord.Member
     except:
         await interaction.response.send_message('*** Error unbanning the member ***')
 
-@client.tree.command(name="ping", description="Check the bot's latency", guild=discord.Object(id=main_guild))
+@client.tree.command(name="ping", description="Check the bot's latency", guild=discord.Object(id=client.main_guild))
 async def slash_command(interaction: discord.Interaction):
     try:
         latency = round(client.latency * 1000)  # Convert latency to milliseconds
@@ -104,4 +104,4 @@ async def slash_command(interaction: discord.Interaction):
 
 
 if __name__ == '__main__':
-    client.run(discord_token)
+    client.run(client.discord_token)
