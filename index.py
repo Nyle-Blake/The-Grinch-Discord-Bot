962e021fc062403583b6c110f80822349e7602d1
Nyle-Blake <Nyleblake1@gmail.com>
2025-02-12 14:02:36 +0000

    Feat: adds additional commands
    Adds kick, ban, unban ,and ping commands.

--- index.py
+++ index.py
@@ -58,5 +58,50 @@ async def slash_command(interaction: discord.Interaction, query: str):
         except:
             await interaction.response.send_message('*** Error summoning the ginch ***')
 
+@client.tree.command(name="kick", description="Kick a member of the server", guild=discord.Object(id=main_guild))
+async def slash_command(interaction: discord.Interaction, member: discord.Member, reason: str):
+    try:
+        if reason == '':
+            await interaction.response.send_message('*** Please provide a reason ***')
+            return
+        await member.send(f'You have been kicked from the server.\n Reason: {reason}')
+        await member.kick()
+        await interaction.response.send_message(f'{member} has been kicked.\n Reason: {reason}')
+    except:
+        await interaction.response.send_message('*** Error kicking the member ***')
 
-client.run(discord_token)
+@client.tree.command(name="ban", description="Ban a member of the server", guild=discord.Object(id=main_guild))
+async def slash_command(interaction: discord.Interaction, member: discord.Member, reason: str):
+    try:
+        if reason == '':
+            await interaction.response.send_message('*** Please provide a reason ***')
+            return
+        await member.send(f'You have been banned from the server.\n Reason: {reason}')
+        await member.ban()
+        await interaction.response.send_message(f'{member} has been banned.\n Reason: {reason}')
+    except:
+        await interaction.response.send_message('*** Error banning the member ***')
+
+@client.tree.command(name="unban", description="Unban a member of the server", guild=discord.Object(id=main_guild))
+async def slash_command(interaction: discord.Interaction, member: discord.Member, reason: str):
+    try:
+        if reason == '':
+            await interaction.response.send_message('*** Please provide a reason ***')
+            return
+        await member.send(f'You have been unbanned from the server.\n Reason: {reason}')
+        await member.unban()
+        await interaction.response.send_message(f'{member} has been unbanned.\n Reason: {reason}')
+    except:
+        await interaction.response.send_message('*** Error unbanning the member ***')
+
+@client.tree.command(name="ping", description="Check the bot's latency", guild=discord.Object(id=main_guild))
+async def slash_command(interaction: discord.Interaction):
+    try:
+        latency = round(client.latency * 1000)  # Convert latency to milliseconds
+        await interaction.response.send_message(f'Ping! Latency is {latency}ms')
+    except:
+        interaction.response.send_message('*** Error checking the latency ***')
+
+
+if __name__ == '__main__':
+    client.run(discord_token)
